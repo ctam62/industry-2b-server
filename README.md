61cd38ed1972aa53b45835e1a05c8bc1c095d081
claratam <claramtam@gmail.com>
2023-11-16 13:40:13 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# API Server
+The backend API server for industry-2b's [frontend application](https://github.com/drouillarda/industry-2b-client).
+
+## Setup
+
+Install dependencies
+```
+npm i
+```
+
+Run App in development
+```
+npm run dev
+```
+
+Start App
+```
+npm start
+```
---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const cors = require("cors");
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
+
+app.get('/', (_req, res) => {
+    res.send("Industry 2b's API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
