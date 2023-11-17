e54bf9d15f3e0cdeecaf3725cabc324ce2569442
claratam <claramtam@gmail.com>
2023-11-17 09:13:52 -0600

    feat: move services logo to logos folder

--- migrations/create_services_table.js
+++ migrations/create_services_table.js
@@ -1,13 +1,13 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('services', (table) => {
-      table.increments('id').primary();
-      table.string('name').notNullable();
-      table.string('logo').defaultTo("/images/netflix.svg");
-      table.string('description').nullable();
-    });
-  };
-  
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable('services');
-  };
+  return knex.schema.createTable('services', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('logo').defaultTo("/logos/netflix.svg");
+    table.string('description').nullable();
+  });
+};
+
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('services');
+};
