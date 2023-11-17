a2aa5677c907c87c7e766b6d0f25cef4cf894ec0
Mariana Capo <macapo78@gmail.com>
2023-11-17 14:40:20 -0500

    added new images

--- seeds/titles.js
+++ seeds/titles.js
@@ -27,5 +27,15 @@ exports.seed = async function (knex) {
     { id: 23, service_id: 1, title: "Sherlock", genres: "Crime, Drama, Mystery, Thriller", rank: 3, img: "/images/sherlock.jpg" },
     { id: 24, service_id: 1, title: "Mindhunter", genres: "Crime, Drama, Thriller", rank: 4, img: "/images/mindhunter.jpg" },
     { id: 25, service_id: 4, title: "The Mandalorian", genres: "Action, Adventure, Fantasy, Sci-Fi", rank: 5, img: "/images/mandalorian.jpg" }
+    { id: 36, service_id: 1, title: "Avatar", genres: "Action, Adventure, Fantasy, Sci-Fi", rank: 21, img: "/images/Avatar.jpeg" },
+    { id: 37, service_id: 4, title: "Narcos", genres: "Biography, Crime, Drama, Thriller", rank: 22, img: "/images/Narcos.jpeg" },
+    { id: 38, service_id: 1, title: "The Princess Bride", genres: "Adventure, Family, Fantasy, Romance", rank: 23, img: "/images/ThePrincessBride.jpeg" },
+    { id: 39, service_id: 2, title: "Deadpool", genres: "Action, Adventure, Comedy, Sci-Fi", rank: 24, img: "/images/Deadpool.jpeg" },
+    { id: 40, service_id: 3, title: "The Witch", genres: "Horror, Mystery", rank: 25, img: "/images/TheWitch.jpeg" },
+    { id: 41, service_id: 2, title: "The Office", genres: "Comedy", rank: 26, img: "/images/TheOffice.jpeg" },
+    { id: 42, service_id: 3, title: "Handmaid's Tale", genres: "Drama, Sci-Fi, Thriller", rank: 27, img: "/images/HandmaidsTale.jpeg" },
+    { id: 43, service_id: 4, title: "Fargo", genres: "Crime, Drama, Thriller", rank: 28, img: "/images/Fargo.jpeg" },
+    { id: 44, service_id: 1, title: "Casablanca", genres: "Drama, Romance, War", rank: 29, img: "/images/Casablanca.jpeg" },
+    { id: 45, service_id: 2, title: "The Great Gatsby", genres: "Drama, Romance", rank: 30, img: "/images/TheGreatGatsby.jpeg" }
   ]);
 };
