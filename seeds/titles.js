42cd5679077f952939e4eaee1bf33cf9b7318bed
claratam <claramtam@gmail.com>
2023-11-16 22:25:19 -0600

    feat: add genres to table

--- seeds/titles.js
+++ seeds/titles.js
@@ -2,15 +2,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('titles').del()
   await knex('titles').insert([
-    { id: 1, title: "The Shawshank Redemption" },
-    { id: 2, title: "The Godfather" },
-    { id: 3, title: "The Dark Knight" },
-    { id: 4, title: "Pulp Fiction" },
-    { id: 5, title: "Schindler's List" },
-    { id: 6, title: "Forrest Gump" },
-    { id: 7, title: "The Matrix" },
-    { id: 8, title: "Inception" },
-    { id: 9, title: "Fight Club" },
-    { id: 10, title: "The Lord of the Rings: The Fellowship of the Ring" }
+    { id: 1, title: "The Shawshank Redemption", genres: "Drama, Crime" },
+    { id: 2, title: "The Godfather", genres: "Crime, Drama" },
+    { id: 3, title: "The Dark Knight", genres: "Action, Crime, Drama" },
+    { id: 4, title: "Pulp Fiction", genres: "Crime, Drama" },
+    { id: 5, title: "Schindler's List", genres: "Biography, Drama, History" },
+    { id: 6, title: "Forrest Gump", genres: "Drama, Romance" },
+    { id: 7, title: "The Matrix", genres: "Action, Sci-Fi" },
+    { id: 8, title: "Inception", genres: "Action, Adventure, Sci-Fi" },
+    { id: 9, title: "Fight Club", genres: "Drama" },
+    { id: 10, title: "The Lord of the Rings: The Fellowship of the Ring", genres: "Action, Adventure, Drama, Fantasy" }
   ]);
 };
